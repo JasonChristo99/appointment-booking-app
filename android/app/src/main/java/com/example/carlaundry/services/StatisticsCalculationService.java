6386e843d84519ee824524b824fc50bf080de3e8
JasonChristo99 <38409798+JasonChristo99@users.noreply.github.com>
2020-04-22 02:12:18 +0300

    Added Statistics util & service

---
+++ android/app/src/main/java/com/example/carlaundry/services/StatisticsCalculationService.java
@@ -0,0 +1,10 @@
+package com.example.carlaundry.services;
+
+import java.time.Period;
+
+public class StatisticsCalculationService {
+    //TODO
+    public static boolean calculateStatistic(Period period) {
+        return false;
+    }
+}
